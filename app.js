7223d96f74d6a12a99879273882645d3ede88017
Ryan Janes <ryanajanes@gmail.com>
2023-01-28 16:41:12 -0800

    if this commit gets pushed the name tag updates based on name input

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 // grab DOM elements
 
 // set event listeners
+console.log('Hello World');
+console.log(42 + 42);
+// variables  
+const dogName = 'Benny';
+console.log(`My dogs name is ${dogName}. ${dogName} is very cute. ${dogName} is 7 years old`);
+
+function sayHello(name) {
+    console.log(`bonjour ${name}`);
+    console.log('it is a beautiful day today');
+    console.log('I hope you are well');
+}
+sayHello ('ryan');
+sayHello ('Jake');
+
+const nameElem = document.getElementById('name');
+nameElem.textContent = 'Ryan';
+
+const updateBtn = document.getElementById('update');
+updateBtn.addEventListener('click', () => {
+    const nameInput = document.getElementById('input');
+    console.log('i am clicking');
+    const nameElem = document.getElementById('name');
+    nameElem.textContent = nameInput.value;
+})
